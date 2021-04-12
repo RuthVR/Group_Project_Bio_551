e54619fa5ea0e1c6967929c1cb6550245d2de169
xerohead <shane.jordan.407@my.csun.edu>
2021-04-12 11:03:45 -0700

    edited m' script

--- first_draft/script/Janitor_Jordan.R
+++ first_draft/script/Janitor_Jordan.R
@@ -6,6 +6,7 @@ library(janitor)
 library(tidyverse)
 library(here)
 library(readxl)
+library(kableExtra)
 
 #bring in data
 coralgrowth<-read.csv("first_draft/Data/CoralGrowth.csv")
@@ -14,6 +15,11 @@ view(coralgrowth) #FYI I renamed the file to be shorter
 #a little data wrangling
 coralsclean<-coralgrowth[,-c(5:14)] #get rid of the analysis on Excel
 view(coralsclean)
+
+#make initial corals table
+coralsclean %>%
+  kbl() %>%
+  kable_classic()
 
 #available functions
 clean_names()
@@ -28,12 +34,22 @@ excel_numeric_to_date() #note we have no date data
 corals<-clean_names(coralgrowth)
 view(corals) #this works, formats the names similarly
 
+#make new corals table
+corals %>%
+  kbl() %>%
+  kable_classic()
+
 #tabyl
-tabyl(corals, change_mg_cm2) #we can put the above object into a frequency table
+coralstable <- tabyl(corals, change_mg_cm2) #we can put the above object into a frequency table
 
 library(dplyr) #we can use a pipe too (requires dplyr)
 corals %>%
   tabyl(change_mg_cm2)
+
+#make the frequency table
+coralstable %>%
+  kbl() %>%
+  kable_classic()
 
 #get_dupes
 get_dupes(corals, change_mg_cm2) #not sure what this has done, 
